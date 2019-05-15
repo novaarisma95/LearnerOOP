aed01d485be0fb318535b8158c21206055166003
Nova Arisma <novaarisma95@gmail.com>
2019-05-15 12:23:15 +0700

    initial commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Nova Arisma on 15/05/19.
+//  Copyright © 2019 Nova Arisma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
